77b42acff90294f0d9abc990d18df1c70748efc0
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-09-18 05:29:33 +0900

    feat: remove serialize & deserialize from condition

--- crates/plint-linter/src/ruleset/condition.rs
+++ crates/plint-linter/src/ruleset/condition.rs
@@ -1,24 +1,13 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug)]
 pub enum Condition {
-    #[serde(rename = "eq")]
-    Eq(String),
-    #[serde(rename = "neq")]
-    Neq(String),
-    #[serde(rename = "gt")]
-    Gt(String),
-    #[serde(rename = "gte")]
-    Gte(String),
-    #[serde(rename = "lt")]
-    Lt(String),
-    #[serde(rename = "lte")]
-    Lte(String),
-    #[serde(rename = "and")]
+    Eq(crate::Value),
+    Neq(crate::Value),
+    Gt(crate::Value),
+    Gte(crate::Value),
+    Lt(crate::Value),
+    Lte(crate::Value),
     And(Vec<Condition>),
-    #[serde(rename = "or")]
     Or(Vec<Condition>),
-    #[serde(rename = "not")]
     Not(Box<Condition>),
 }
 
@@ -82,20 +71,20 @@ impl Condition {
 
 fn comp_value(
     a: &crate::Value,
-    b: &str,
+    b: &crate::Value,
     cmp_str: impl FnOnce(&str, &str) -> bool,
     cmp_i64: impl FnOnce(i64, i64) -> bool,
     cmp_f64: impl FnOnce(f64, f64) -> bool,
     cmp_bool: impl FnOnce(bool, bool) -> bool,
 ) -> bool {
     match a {
-        crate::Value::String(a_str) => cmp_str(a_str, b),
-        crate::Value::Integer(a_int) => b.parse::<i64>().is_ok_and(|b_int| cmp_i64(*a_int, b_int)),
+        crate::Value::String(a_str) => cmp_str(a_str, &b.force_string()),
+        crate::Value::Integer(a_int) => b.try_i64().is_some_and(|b_int| cmp_i64(*a_int, b_int)),
         crate::Value::Float(a_float) => b
-            .parse::<f64>()
-            .is_ok_and(|b_float| cmp_f64(*a_float, b_float)),
-        crate::Value::Boolean(a_bool) => b
-            .parse::<bool>()
-            .is_ok_and(|b_bool| cmp_bool(*a_bool, b_bool)),
+            .try_f64()
+            .is_some_and(|b_float| cmp_f64(*a_float, b_float)),
+        crate::Value::Boolean(a_bool) => {
+            b.try_bool().is_some_and(|b_bool| cmp_bool(*a_bool, b_bool))
+        }
     }
 }
