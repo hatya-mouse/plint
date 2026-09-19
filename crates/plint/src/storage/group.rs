c600916d70951f6635c309161ac859b117db7c93
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-09-19 23:53:39 +0900

    feat(storage): add save and remove function to Graph

--- crates/plint/src/storage/group.rs
+++ crates/plint/src/storage/group.rs
@@ -1,4 +1,4 @@
-use crate::{PlintIoError, storage::IndexFile};
+use crate::{PlintIoError, storage::IndexFile, utils::groups_dir};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -16,5 +16,65 @@ impl Group {
         // Read the group file
         let group_string = std::fs::read_to_string(&group_path).map_err(PlintIoError::IoError)?;
         yaml_serde::from_str::<Group>(&group_string).map_err(PlintIoError::YamlParseError)
+    }
+
+    /// Saves the group to the storage, adding it to the index file if it doesn't already exist.
+    /// This overwrites the existing group file if it already exists.
+    pub(crate) fn save(&self) -> Result<(), PlintIoError> {
+        let mut index_file = IndexFile::load()?;
+
+        let Some(group_path) =
+            groups_dir().map(|path| path.join(&self.name).with_added_extension("yaml"))
+        else {
+            return Err(PlintIoError::PathNotAvailable);
+        };
+
+        // Register the path to the newly created ruleset file in the index file
+        let original_entry = index_file
+            .groups
+            .insert(self.name.clone(), group_path.clone());
+        IndexFile::save(&index_file)?;
+
+        let result = match yaml_serde::to_string(self) {
+            Ok(group_string) => match std::fs::write(group_path, group_string) {
+                Ok(_) => Ok(()),
+                Err(err) => Err(PlintIoError::IoError(err)),
+            },
+            Err(err) => Err(PlintIoError::YamlParseError(err)),
+        };
+
+        // In case of an error, revert the entry from the index file
+        if result.is_err() {
+            if let Some(original_entry) = original_entry {
+                index_file.groups.insert(self.name.clone(), original_entry);
+            } else {
+                index_file.groups.remove(&self.name);
+            }
+            IndexFile::save(&index_file)?;
+        }
+
+        result
+    }
+
+    /// Removes the group with the name.
+    fn remove(name: &str) -> Result<(), PlintIoError> {
+        let mut index_file = IndexFile::load()?;
+
+        // Remove the ruleset entry from the index file
+        let group_path = index_file
+            .groups
+            .remove(name)
+            .ok_or_else(|| PlintIoError::NotFound(name.to_string()))?;
+
+        // Remove the ruleset file from the filesystem
+        match std::fs::remove_file(&group_path) {
+            Ok(_) => (),
+            Err(err) => {
+                index_file.groups.insert(name.to_string(), group_path);
+                return Err(PlintIoError::IoError(err));
+            }
+        }
+
+        IndexFile::save(&index_file)
     }
 }
--- crates/plint/src/storage/mod.rs
+++ crates/plint/src/storage/mod.rs
@@ -1,10 +1,11 @@
 mod group;
 mod index;
+mod ruleset;
 
 pub(super) use group::Group;
 pub(super) use index::IndexFile;
+pub(super) use ruleset::RulesetIo;
 
-use crate::utils::rulesets_dir;
 use plint_linter::Ruleset;
 
 #[derive(Debug)]
@@ -13,89 +14,6 @@ pub(super) enum PlintIoError {
     NotFound(String),
     YamlParseError(yaml_serde::Error),
     IoError(std::io::Error),
-}
-
-pub(super) trait RulesetIo {
-    /// Loads the ruleset with the given name from the data directory.
-    fn load(name: &str) -> Result<Ruleset, PlintIoError>;
-
-    /// Saves the ruleset to the storage, adding it to the index file if it doesn't already exist.
-    /// This overwrites the existing ruleset file if it already exists.
-    fn save(&self) -> Result<(), PlintIoError>;
-
-    /// Removes the ruleset with the given name.
-    fn remove(name: &str) -> Result<(), PlintIoError>;
-}
-
-impl RulesetIo for Ruleset {
-    fn load(name: &str) -> Result<Ruleset, PlintIoError> {
-        // Get the ruleset path from the index file
-        let ruleset_path = IndexFile::load()?.ruleset_path(name)?;
-
-        // Read the ruleset file
-        let ruleset_string =
-            std::fs::read_to_string(&ruleset_path).map_err(PlintIoError::IoError)?;
-        yaml_serde::from_str::<Ruleset>(&ruleset_string).map_err(PlintIoError::YamlParseError)
-    }
-
-    fn save(&self) -> Result<(), PlintIoError> {
-        let mut index_file = IndexFile::load()?;
-
-        let Some(ruleset_path) =
-            rulesets_dir().map(|path| path.join(&self.name).with_added_extension("yaml"))
-        else {
-            return Err(PlintIoError::PathNotAvailable);
-        };
-
-        // Register the path to the newly created ruleset file in the index file
-        let original_entry = index_file
-            .rulesets
-            .insert(self.name.clone(), ruleset_path.clone());
-        IndexFile::save(&index_file)?;
-
-        let result = match yaml_serde::to_string(self) {
-            Ok(ruleset_string) => match std::fs::write(ruleset_path, ruleset_string) {
-                Ok(_) => Ok(()),
-                Err(err) => Err(PlintIoError::IoError(err)),
-            },
-            Err(err) => Err(PlintIoError::YamlParseError(err)),
-        };
-
-        // In case of an error, revert the entry from the index file
-        if result.is_err() {
-            if let Some(original_entry) = original_entry {
-                index_file
-                    .rulesets
-                    .insert(self.name.clone(), original_entry);
-            } else {
-                index_file.rulesets.remove(&self.name);
-            }
-            IndexFile::save(&index_file)?;
-        }
-
-        result
-    }
-
-    fn remove(name: &str) -> Result<(), PlintIoError> {
-        let mut index_file = IndexFile::load()?;
-
-        // Remove the ruleset entry from the index file
-        let ruleset_path = index_file
-            .rulesets
-            .remove(name)
-            .ok_or_else(|| PlintIoError::NotFound(name.to_string()))?;
-
-        // Remove the ruleset file from the filesystem
-        match std::fs::remove_file(&ruleset_path) {
-            Ok(_) => (),
-            Err(err) => {
-                index_file.rulesets.insert(name.to_string(), ruleset_path);
-                return Err(PlintIoError::IoError(err));
-            }
-        }
-
-        IndexFile::save(&index_file)
-    }
 }
 
 // --- MULTIPLE RULESETS ---
