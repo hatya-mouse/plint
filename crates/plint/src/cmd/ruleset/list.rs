9e9928c903ce688934fcacee2dac215e2cf3c042
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-09-20 00:27:33 +0900

    feat: show the path in a single line in ruleset list

--- crates/plint/src/cmd/ruleset/list.rs
+++ crates/plint/src/cmd/ruleset/list.rs
@@ -10,12 +10,12 @@ pub(crate) fn list(path: bool, path_nolink: bool) {
     };
 
     for (ruleset_name, ruleset_path) in index_file.rulesets {
-        println!("{}", ruleset_name);
-
         if path_nolink {
-            println!("  {}", ruleset_path.display());
+            println!("{}: {}", ruleset_name, ruleset_path.display());
         } else if path {
-            println!("  {}", hyperlink(&ruleset_path));
+            println!("{}: {}", ruleset_name, hyperlink(&ruleset_path));
+        } else {
+            println!("{}", ruleset_name);
         }
     }
 }
