00c68fd9d0f57e357c3fc71e34c09609faf99b1d
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-09-20 18:18:03 +0900

    feat(lang): implement parser abstract syntax graph

---
+++ crates/plint-linter/src/lang/ast.rs
@@ -0,0 +1,32 @@
+use crate::Match;
+
+pub(super) enum Expression {
+    FunctionCall {
+        name: String,
+        args: Vec<Expression>,
+    },
+    Variable {
+        name: String,
+    },
+    For {
+        variable: String,
+        iterable: Box<Expression>,
+        body: Vec<Expression>,
+    },
+    If {
+        main: IfArm,
+        else_ifs: Vec<IfArm>,
+        else_body: Vec<Expression>,
+    },
+}
+
+pub(super) struct IfArm {
+    pub(crate) condition: Box<Expression>,
+    pub(crate) body: Vec<Expression>,
+}
+
+pub(crate) enum LangValue {
+    Match(Match),
+    Number(f64),
+    String(String),
+}
