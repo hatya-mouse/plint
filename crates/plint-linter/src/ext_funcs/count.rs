47de3da872ff157533899d51313bbf7f0f1886db
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-09-21 15:22:43 +0900

    feat(linter): add len func to count items in vec

--- crates/plint-linter/src/ext_funcs/count.rs
+++ crates/plint-linter/src/ext_funcs/count.rs
@@ -2,10 +2,19 @@ use plint_lang::{Interpreter, Value};
 use unicode_segmentation::UnicodeSegmentation;
 
 pub(super) fn add_funcs(interpreter: &mut Interpreter) {
+    interpreter.add_func("len", Box::new(len));
     interpreter.add_func("char_count", Box::new(char_count));
     interpreter.add_func("byte_count", Box::new(byte_count));
     interpreter.add_func("word_count", Box::new(word_count));
     interpreter.add_func("line_count", Box::new(line_count));
+}
+
+fn len(args: &[Value]) -> Result<Value, String> {
+    if let [Value::List(list)] = args {
+        Ok(Value::Integer(list.len().try_into().unwrap_or_default()))
+    } else {
+        Err("Invalid arguments for len function".to_string())
+    }
 }
 
 fn char_count(args: &[Value]) -> Result<Value, String> {
